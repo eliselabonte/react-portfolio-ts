ee8a0c89074298e7d59bb2dc94f45911590d9aa5
Elise_LaBonte <emlabonte714@gmail.com>
2021-11-08 18:33:11 -0500

    added components and styling

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,5 +1,23 @@
-function Portfolio()  {
+import React, { useState } from 'react';
+import PortfolioItem from './PortfolioItem'
 
-};
+function Portfolio()  {
+    const [item, setItem] = useState([{
+        name: '',
+        deployed: '',
+        gitHub: '',
+        image: ''
+    }])
+
+return (
+    <div>
+        <h1>Welcome to my portfolio</h1>
+        <div className='blue' >
+        <PortfolioItem />
+        </div>
+    </div>
+)
+}
+
 
 export default Portfolio;
