ee12579f4255720dd43e9b7911b2e73d5312049d
Elise_LaBonte <emlabonte714@gmail.com>
2021-11-11 10:05:40 -0500

    remove bulma

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import Header from './components/Header';
 import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
-import 'bulma/css/bulma.min.css';
 
 function App() {
   return (
