1b9e8615e2865e678cb3d2d406c29ce751196d7c
Elise_LaBonte <emlabonte714@gmail.com>
2021-11-16 13:24:05 -0500

    moved project data, added styling to projects, fixed z-index of nav bar

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,16 +1,89 @@
-import React, { useState } from 'react';
-import Project from './Project'
-import projects from './properties';
-import './portfolio.scss'
-// TODO: not sure if projects get used here or in item
+// import { VscGithub } from "react-icons/fa";
+import './portfolio.scss';
 
-export default function Portfolio()  {
+export default function Portfolio()   {
+    const projects =    [
+        {
+            id: 1,
+            name: 'Code Quiz',
+            deployed: 'https://eliselabonte.github.io/timed-coding-quiz/',
+            gitHub: '',
+            findBy: 'code-quiz'
+        },
+        {
+            id: 2,
+            name: 'Art Portfolio',
+            deployed: 'https://codepen.io/elise-labonte/full/bGgrKjB',
+            gitHub: '',
+            findBy: 'art'
+        },
+        {
+            id: 3,
+            name: 'Crypt-ID',
+            deployed: 'https://polar-earth-58321.herokuapp.com/',
+            gitHub: '',
+            findBy: 'cryptid'
+        },
+        {
+            id: 4,
+            name: 'Who\'s Your Daddy?',
+            deployed: 'https://eliselabonte.github.io/whos-your-daddy/',
+            gitHub: '',
+            findBy: 'dad'
+        },
+        {
+            id: 5,
+            name: 'Password Generator',
+            deployed: 'https://eliselabonte.github.io/createAPassword/',
+            gitHub: '',
+            findBy: 'password'
+        },
+        {
+            id: 6,
+            name: 'Weather Application',
+            deployed: 'https://eliselabonte.github.io/weather-app/',
+            gitHub: '',
+            findBy: 'weather-app'
+        },
+        {
+            id: 7,
+            name: 'Productivity Tracker',
+            deployed: 'https://eliselabonte.github.io/work-tracker/',
+            gitHub: '',
+            findBy: 'work-tracker'
+        }
+    ]
+
+    const item1 = projects[0];
+    const item2 = projects[1];
+    const item3 = projects[2];
+
     return (
-            <div>
-                <Project />
+        <div className='projectContainer'>
+            <div className={`${item1.findBy} projectItem`}>
+                <h4>{item1.name}</h4>
+                <div className='links'>
+                    <a href={item1.gitHub}>🙀</a>
+                    <a href={item1.deployed}>🚀</a>
+                </div>
             </div>
+            <div className={`${item2.findBy} projectItem`}>
+                <h4>{item2.name}</h4>
+                <div className='links'>
+                    <a href={item2.gitHub}>🙀</a>
+                    <a href={item2.deployed}>🚀</a>
+                </div>
+            </div>
+            <div className={`${item3.findBy} projectItem`}>
+                <h4>{item3.name}</h4>
+                <div className='links'>
+                    <a href={item3.gitHub}>🙀</a>
+                    <a href={item3.deployed}>🚀</a>
+                </div>
+            </div>
+            
+        </div>
     )
+
+    
 }
-
-
-// holds portfolio items
